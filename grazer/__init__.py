84a103433bba1a87c17c510723db66968498817a
spiceworm <me@spiceworm.xyz>
2024-02-27 15:48:13 -0700

    Use concurrent.futures instead
    Much faster

--- grazer/__init__.py
+++ grazer/__init__.py
@@ -1,45 +1,23 @@
-import aiohttp
-import asyncio
+import concurrent.futures
 
 import bs4
+import requests
 import yaml
 
 
-async def consumer(q):
-    while True:
-        try:
-            yield await asyncio.wait_for(q.get(), 5)
-        except (asyncio.QueueEmpty, asyncio.TimeoutError):
-            break
-
-
-async def worker(label, element, session, q):
+def get_price(label, element):
     url = element["url"]
     css_selector = element["css_selector"]
-    html_key = element["html_key"]
+    html_key = element.get("html_key")
 
-    async with session.get(url) as resp:
-        if resp.ok:
-            text = await resp.read()
-            soup = bs4.BeautifulSoup(text, features="html.parser")
-            selected = soup.select(css_selector)
-            result = selected[0].get(html_key)
-            await q.put({label: result})
-
-
-async def execute(elements: dict):
-    q = asyncio.Queue(maxsize=0)
-
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for label, element in elements.items():
-            task = asyncio.create_task(worker(label, element, session, q))
-            tasks.append(task)
-
-        async for result in consumer(q):
-            print(result)
-
-        await asyncio.gather(*tasks)
+    resp = requests.get(url)
+    soup = bs4.BeautifulSoup(resp.text, features="html.parser")
+    selected = soup.select(css_selector)[0]
+    if html_key:
+        result = selected.get(html_key)
+    else:
+        result = selected.text
+    return {label: result}
 
 
 def graze(config: str | None = None, elements: dict | None = None):
@@ -48,7 +26,6 @@ def graze(config: str | None = None, elements: dict | None = None):
         with open(config) as f:
             _elements.update(yaml.safe_load(f))
 
-    import time
-    start = time.time()
-    asyncio.run(execute(_elements))
-    print(f'ran in {time.time() - start} seconds')
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_price, label, element) for label, element in _elements.items()]
+        yield from (f.result() for f in concurrent.futures.as_completed(futures))
